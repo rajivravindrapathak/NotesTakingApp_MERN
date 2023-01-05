fadfc921fb17bf51dabe9ab2c6e868ff10752b05
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-01-05 22:10:22 +0530

    Add files via upload

---
+++ backened/models/Note.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const noteSchema = new mongoose.Schema ({
+    Heading: {type: String, required: true},
+    Note: {type: String, required: true},
+    Tag: {type: String, required: true}
+})
+
+const NoteModel = mongoose.model("note", noteSchema)
+
+module.exports = { NoteModel }
---
+++ backened/models/User.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema ({
+    email: {type: String, required: true},
+    passwoard: {type: String, required: true},
+    age: {type: Number, required: true}
+})
+
+const UserModel = mongoose.model("user", userSchema)
+
+module.exports = { UserModel }
