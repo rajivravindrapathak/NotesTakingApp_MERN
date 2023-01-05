a1923d567d18da3b72fc12d2deb8fb5ac5d69951
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-01-05 22:16:28 +0530

    Add files via upload

--- backened/server.js
+++ backened/server.js
@@ -1,1 +1,30 @@
+const express = require("express")
 
+const {userController} = require("./routes/user.routes")
+const {notesController} = require("./routes/notes.routes")
+
+const { connection } = require("./config/db")
+
+const app = express()
+const PORT = 8080
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("home page")
+})
+
+app.use("/user", userController)
+
+app.use("/notes", notesController)
+
+app.listen(PORT, async ()=> {
+    try {
+        await connection;
+        console.log("connected to DB")
+    } catch (err) {
+        console.log("Error connected to db")
+        console.log(err)
+    }
+    console.log(`listning on PORT ${PORT}`)
+})
