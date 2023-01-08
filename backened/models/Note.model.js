2844d9e124c8e5e0b9d4a7fc2a9076f9d6b4d31b
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2023-01-08 23:00:04 +0530

    Add files via upload

--- backened/models/Note.model.js
+++ backened/models/Note.model.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose")
 const noteSchema = new mongoose.Schema ({
     Heading: {type: String, required: true},
     Note: {type: String, required: true},
-    Tag: {type: String, required: true}
+    Tag: {type: String, required: true},
+    userId: {type: String, required: true}
 })
 
 const NoteModel = mongoose.model("note", noteSchema)
--- backened/models/User.model.js
+++ backened/models/User.model.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema ({
     email: {type: String, required: true},
     passwoard: {type: String, required: true},
     age: {type: Number, required: true}
+    
 })
 
 const UserModel = mongoose.model("user", userSchema)
